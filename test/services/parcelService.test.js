03126d8d115a6e410061b278c119692b8e24c4e6
David Bingham <david.bingham@defra.gov.uk>
2020-01-09 16:39:09 +0000

    Remove duplication in parcelService tests

--- test/services/parcelService.test.js
+++ test/services/parcelService.test.js
@@ -1,21 +1,23 @@
 const parcelService = require('../../server/services/parcelService')
 
 describe('parcelService', () => {
-  const parcelShape = expect.objectContaining({
+  const parcelShape = {
     id: expect.stringMatching(new RegExp('.+'))
-  })
+  }
 
   test('returns a JSON collection of parcel objects', () => {
     const result = parcelService.get()
 
-    expect(result).toEqual(expect.arrayContaining([parcelShape]))
+    expect(result).toEqual(expect.arrayContaining([
+      expect.objectContaining(parcelShape)
+    ]))
   })
 
   test('response does not contain invalid items', () => {
     const result = parcelService.get()
 
-    expect(result).not.toEqual(expect.arrayContaining([expect.not.objectContaining({
-      id: expect.stringMatching(new RegExp('.+'))
-    })]))
+    expect(result).not.toEqual(expect.arrayContaining([
+      expect.not.objectContaining(parcelShape)
+    ]))
   })
 })
