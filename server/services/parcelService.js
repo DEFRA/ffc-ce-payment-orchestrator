23212b9608c5461542eaed4bc5ce06249b713859
David Bingham <david.bingham@defra.gov.uk>
2020-01-09 16:24:31 +0000

    Move parcel data to a JSON file

--- server/services/parcelService.js
+++ server/services/parcelService.js
@@ -1,12 +1,7 @@
+const parcels = require('../../data/parcels.json')
+
 module.exports = {
   get: function () {
-    return [
-      { id: '111111' },
-      { id: '222222' },
-      { id: '333333' },
-      { id: '444444' },
-      { id: '555555' },
-      { id: '666666' }
-    ]
+    return parcels
   }
 }
