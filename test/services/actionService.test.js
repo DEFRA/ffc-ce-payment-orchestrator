615352c0da80e4ba7ea157e1a0d69c9cec397233
Matthew Collins <matthew.collins@defra.gov.uk>
2020-01-10 16:15:45 +0000

    Dealing with PR comments

--- test/services/actionService.test.js
+++ test/services/actionService.test.js
@@ -1,14 +1,12 @@
 
 const actionsService = require('../../server/services/actionsService')
 describe('actionsService', () => {
-  test('actions service returns a actions object containing an array', async () => {
-    expect.assertions(1)
+  test('actions service returns an array', async () => {
     const actions = await actionsService.get()
     expect(Array.isArray(actions)).toEqual(true)
   })
 
-  test('actions service object contains an array where the first element has an id of 1', async () => {
-    expect.assertions(1)
+  test('actions service returns an array where the first element has an id of 1', async () => {
     const actions = await actionsService.get()
     expect(actions[0]).toMatchObject({
       id: 'FG1',
--- test/services/rulesService.test.js
+++ test/services/rulesService.test.js
@@ -1,14 +1,12 @@
 
 const rulesService = require('../../server/services/rulesService')
 describe('rulesService', () => {
-  test('rules service returns a rules object containing an array', async () => {
-    expect.assertions(1)
+  test('rules service returns an array', async () => {
     const rules = await rulesService.get()
     expect(Array.isArray(rules)).toEqual(true)
   })
 
-  test('rules service object contains an array where the first element has an id of 1', async () => {
-    expect.assertions(1)
+  test('rules service returns an array where the first element has an id of 1', async () => {
     const rules = await rulesService.get()
     expect(rules[0]).toMatchObject({ id: 1 })
   })
