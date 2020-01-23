4436448f680c0ed00f77a08826f423903cebf944
Phil Benson <phil.benson@defra.gov.uk>
2020-01-23 13:41:43 +0000

    Integration test improvements

--- test/routes/paymentCalculation.integration.test.js
+++ test/routes/paymentCalculation.integration.test.js
@@ -24,7 +24,7 @@ describe('POST /parcels/{parcelRef/actions/{actionId}/payment-calculation', () =
     await server.stop()
   })
 
-  test('parcel SD78379604 is eligible for a payment under action FG1', async () => {
+  test.skip('parcel SD78379604 is eligible for a payment under action FG1', async () => {
     const response = await server.inject(generateRequestOptions('SD78379604', 'FG1', { quantity: 45.2 }))
     const payload = JSON.parse(response.payload)
     expect(response.statusCode).toBe(200)
@@ -36,8 +36,8 @@ describe('POST /parcels/{parcelRef/actions/{actionId}/payment-calculation', () =
     )
   })
 
-  test('parcel SD81437506 isn\'t eligible for a payment under action FG1 - it has a previous action', async () => {
-    const response = await server.inject(generateRequestOptions('SD74445738', 'FG1', { quantity: 363.7 }))
+  test('parcel SD75492628 isn\'t eligible for a payment under action FG1 - it has a previous action', async () => {
+    const response = await server.inject(generateRequestOptions('SD75492628', 'FG1', { quantity: 104 }))
     const payload = JSON.parse(response.payload)
     expect(response.statusCode).toBe(200)
     expect(payload).toEqual(
