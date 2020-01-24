907a914e0e3d4713d90e2a37864cc5c422bda81b
Matthew Collins <matthew.collins@defra.gov.uk>
2020-01-24 13:21:03 +0000

    Adding parcelActionsService change with mock for eligibility rules

--- server/services/parcelActionsService.js
+++ server/services/parcelActionsService.js
@@ -1,7 +1,30 @@
-const actions = require('../../data/actions.json')
+const actionsService = require('./actionsService')
+const parcelService = require('./parcelService')
+const rulesEngine = require('../rules-engine')
 
 module.exports = {
   get: async function (parcelRef) {
-    return actions
+    const actions = actionsService.get()
+    const returnActions = []
+    const parcelData = parcelService.getByRef(parcelRef)
+    // Mock rules engine function
+    rulesEngine.doEligibilityRun = rulesEngine.doFullRun
+    // End of mock
+    for (const action of actions) {
+      rulesEngine.resetEngine()
+      let actionPassed = false
+      const successFunc = function () {
+        actionPassed = true
+      }
+      // Mock rules engine function
+      const rulesToUse = rulesEngine.enabledEligibilityRules(action.rules)
+      //
+      await rulesEngine.doEligibilityRun(rulesToUse, [parcelData], { }, successFunc)
+      if (actionPassed) {
+        delete action.rules
+        returnActions.push(action)
+      }
+    }
+    return returnActions
   }
 }
--- test/services/parcelActionsService.test.js
+++ test/services/parcelActionsService.test.js
@@ -1,17 +1,95 @@
+const mockParcelsToReturn = [{
+  ref: 'SS12345678',
+  SSSI: false
+},
+{
+  ref: 'XX12345678',
+  SSSI: true
+}
+]
+jest.mock('../../server/services/parcelService', () => {
+  return {
+    getByRef: jest.fn((parcelRef) => {
+      return mockParcelsToReturn.find(x => x.ref === parcelRef)
+    })
+  }
+})
+jest.mock('../../server/services/actionsService', () => {
+  return {
+    get: jest.fn(() => {
+      return [{
+        id: 'TE1',
+        description: 'Testing',
+        rules: [
+          {
+            id: 5,
+            type: 'eligibility',
+            types: ['eligibility'],
+            description: 'Parcel within SSSI',
+            enabled: true,
+            facts: [
+              {
+                id: 'SSSI',
+                description: 'Parcel in SSSI area'
+              }
+            ],
+            conditions: [{
+              fact: 'SSSI',
+              operator: 'equal',
+              value: true
+            }]
+          }]
+      },
+      {
+        id: 'TE2',
+        description: 'Testing',
+        rules: [
+          {
+            id: 6,
+            type: 'eligibility',
+            types: ['eligibility'],
+            description: 'Parcel not within SSSI',
+            enabled: true,
+            facts: [
+              {
+                id: 'SSSI',
+                description: 'Parcel in SSSI area'
+              }
+            ],
+            conditions: [{
+              fact: 'SSSI',
+              operator: 'notEqual',
+              value: true
+            }]
+          }]
+      }]
+    })
+  }
+})
+
 const parcelActionsService = require('../../server/services/parcelActionsService')
-const parcelRef = 'AB12345678'
+const SSSIparcelRef = 'SS12345678'
+const nonSSSIparcelRef = 'XX12345678'
 
 describe('parcelActionService', () => {
   test('parcel actions service returns an array', async () => {
-    const actions = await parcelActionsService.get(parcelRef)
+    const actions = await parcelActionsService.get(SSSIparcelRef)
     expect(Array.isArray(actions)).toEqual(true)
   })
 
-  test('parcel actions service returns an array where the first element has an id of FG1', async () => {
-    const actions = await parcelActionsService.get(parcelRef)
+  test('for an SSSI disabled parcel actions service returns an array where the first element has an id of TE1', async () => {
+    const actions = await parcelActionsService.get(SSSIparcelRef)
     expect(actions[0]).toMatchObject({
-      id: 'FG1',
-      description: 'Fencing'
+      id: 'TE1',
+      description: 'Testing'
+    })
+  })
+
+  test('for an SSSI enable parcel actions service returns an array where the first element has an id of TE2', async () => {
+    const actions = await parcelActionsService.get(nonSSSIparcelRef)
+    expect(actions[0]).toMatchObject({
+      id: 'TE2',
+      description: 'Testing'
     })
   })
 })
