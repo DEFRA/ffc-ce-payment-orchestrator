dcda90741390f418532581d8a7cd000b683cb366
Matthew Collins <matthew.collins@defra.gov.uk>
2020-01-27 09:32:42 +0000

    Merge in new eligibility rules code

--- server/services/parcelActionsService.js
+++ server/services/parcelActionsService.js
@@ -7,19 +7,13 @@ module.exports = {
     const actions = actionsService.get()
     const returnActions = []
     const parcelData = parcelService.getByRef(parcelRef)
-    // Mock rules engine function
-    rulesEngine.doEligibilityRun = rulesEngine.doFullRun
-    // End of mock
     for (const action of actions) {
       rulesEngine.resetEngine()
       let actionPassed = false
       const successFunc = function () {
         actionPassed = true
       }
-      // Mock rules engine function
-      const rulesToUse = rulesEngine.enabledEligibilityRules(action.rules)
-      //
-      await rulesEngine.doEligibilityRun(rulesToUse, [parcelData], { }, successFunc)
+      await rulesEngine.doEligibilityRun(action.rules, [parcelData], { }, successFunc)
       if (actionPassed) {
         delete action.rules
         returnActions.push(action)
