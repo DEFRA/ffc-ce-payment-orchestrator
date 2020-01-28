18da00e281abe57ce5bcec0ea8a9761538d82806
Matthew Collins <matthew.collins@defra.gov.uk>
2020-01-28 12:28:01 +0000

    Add new rule, plus tests

--- test/rules-engine/area.test.js
+++ test/rules-engine/area.test.js
@@ -2,7 +2,11 @@ describe('Rules engine Area rule test', () => {
   let rulesEngine
   const parcelsTestData = require('./test-data/parcels-area.json')
   const testRules = require('./test-data/rules-area.json')
+<<<<<<< HEAD
   const areaParcelRefs = ['SD75492628', 'SD78379604', 'SD81437506']
+=======
+  const areaParcelRefs = ['SD78379604', 'SD81437506']
+>>>>>>> Add new rule, plus tests
 
   beforeAll(() => {
     rulesEngine = require('../../server/rules-engine')
@@ -12,15 +16,25 @@ describe('Rules engine Area rule test', () => {
     rulesEngine.resetEngine()
   })
 
+<<<<<<< HEAD
   test('area rule discards parcels with an usable area too low', async () => {
+=======
+  test('area rule discards parcels with an action date in the last 5 years', async () => {
+>>>>>>> Add new rule, plus tests
     const acceptedParcels = []
     const successFunc = async function (event, almanac, ruleResult) {
       const ref = await almanac.factValue('ref')
       acceptedParcels.push(ref)
     }
+<<<<<<< HEAD
     await rulesEngine.doFullRun(testRules, parcelsTestData, { quantity: 0.6 }, successFunc)
     expect(acceptedParcels).toHaveLength(areaParcelRefs.length)
     expect(acceptedParcels).toEqual(expect.arrayContaining(areaParcelRefs))
+=======
+    await rulesEngine.doFullRun(testRules, parcelsTestData, { quantity: 1000 }, successFunc)
+    expect(acceptedParcels).toHaveLength(areaParcelRefs.length)
+    expect(acceptedParcels).arrayContaining(areaParcelRefs)
+>>>>>>> Add new rule, plus tests
   })
 
   afterEach(() => {
