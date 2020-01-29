2742c066eeca3cbd8a46140d1edff19a9bf04cb7
Matthew Collins <matthew.collins@defra.gov.uk>
2020-01-29 11:09:56 +0000

    Rule added and test updated

--- test/rules-engine/area.test.js
+++ test/rules-engine/area.test.js
@@ -1,29 +1,41 @@
 describe('Rules engine Area rule test', () => {
   let rulesEngine
   const parcelsTestData = require('./test-data/parcels-area.json')
-  const testRules = require('./test-data/rules-area.json')
-  const areaParcelRefs = ['SD75492628', 'SD78379604', 'SD81437506']
+  const ruleService = require('../../server/services/rulesService')
+  let testRules
 
-  beforeAll(() => {
+  beforeAll(async () => {
     rulesEngine = require('../../server/rules-engine')
+    testRules = await ruleService.get()
   })
 
   beforeEach(() => {
     rulesEngine.resetEngine()
   })
 
-  test('area rule discards parcels with an usable area too low', async () => {
+  test('"area rule excluding features" discards parcels with an usable area too low', async () => {
+    const areaParcelRefs = ['SD75492628', 'SD78379604', 'SD81437506']
     const acceptedParcels = []
     const successFunc = async function (event, almanac, ruleResult) {
       const ref = await almanac.factValue('ref')
       acceptedParcels.push(ref)
     }
-    await rulesEngine.doFullRun(testRules, parcelsTestData, { quantity: 0.6 }, successFunc)
+    const rule = { enabled: true, ...testRules.find(x => x.id === 6) }
+    await rulesEngine.doFullRun([rule], parcelsTestData, { quantity: 0.6 }, successFunc)
     expect(acceptedParcels).toHaveLength(areaParcelRefs.length)
     expect(acceptedParcels).toEqual(expect.arrayContaining(areaParcelRefs))
   })
 
-  afterEach(() => {
-    jest.clearAllMocks()
+  test('"area rule" discards parcels with an usable area too low but ignores features', async () => {
+    const areaParcelRefs = ['SD74445738', 'SD75492628', 'SD78379604', 'SD81437506']
+    const acceptedParcels = []
+    const successFunc = async function (event, almanac, ruleResult) {
+      const ref = await almanac.factValue('ref')
+      acceptedParcels.push(ref)
+    }
+    const rule = { enabled: true, ...testRules.find(x => x.id === 7) }
+    await rulesEngine.doFullRun([rule], parcelsTestData, { quantity: 0.6 }, successFunc)
+    expect(acceptedParcels).toHaveLength(areaParcelRefs.length)
+    expect(acceptedParcels).toEqual(expect.arrayContaining(areaParcelRefs))
   })
 })
