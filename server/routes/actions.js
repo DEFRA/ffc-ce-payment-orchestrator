81d71afa40aff363391760e0cb58f71995457567
Matthew Collins <matthew.collins@defra.gov.uk>
2020-01-30 10:57:30 +0000

    Add precheck PUT handler

--- server/routes/actions.js
+++ server/routes/actions.js
@@ -1,12 +1,39 @@
+const paramsSchema = require('../schema/actionParams')
+const payloadSchema = require('../schema/actionPayload')
 const actionsService = require('../services/actionsService')
 
-module.exports = {
-  method: 'GET',
-  path: '/actions',
-  options: {
-    handler: async (request, h) => {
-      const actions = await actionsService.get()
-      return h.response({ actions }).code(200)
+module.exports = [
+  {
+    method: 'GET',
+    path: '/actions',
+    options: {
+      handler: async (request, h) => {
+        const actions = await actionsService.get()
+        return h.response({ actions }).code(200)
+      }
+    }
+  },
+  {
+    method: 'PUT',
+    path: '/actions/{actionID}',
+    options: {
+      validate: {
+        params: paramsSchema,
+        payload: payloadSchema,
+        failAction: async (request, h, error) => {
+          console.log('actions: failed validation')
+          console.log(`actions: actionID ${request.params.actionID}`)
+          console.log('actions: payload', request.payload)
+          return h.response().code(400).takeover()
+        }
+      },
+      handler: async (request, h) => {
+        const actionID = request.params.actionID
+        const enabled = request.payload.enabled
+        console.log(`actionRules: set pre-check on action ${actionID} to ${enabled}`)
+        const updatedRule = await actionsService.updatePrecheckEnabled(actionID, enabled)
+        return updatedRule ? h.response(updatedRule).code(200) : h.response().code(400)
+      }
     }
   }
-}
+]
---
+++ test/routes/actionPrecheck.test.js
@@ -0,0 +1,74 @@
+describe('Action precheck put route test', () => {
+  let createServer
+  let server
+
+  const actionID = 'TE1'
+
+  const goodRequest = {
+    method: 'PUT',
+    url: `/actions/${actionID}`,
+    payload: { enabled: true }
+  }
+
+  const badRequestPayload = {
+    method: 'PUT',
+    url: '/actions/TE1',
+    payload: { enabled: '123' }
+  }
+
+  const mockUpdatedAction = {
+    id: 'TE1',
+    description: 'Test action',
+    rate: 1,
+    precheck: true,
+    rules: [
+      {
+        id: 1,
+        enabled: true
+      }
+    ]
+  }
+
+  const mockActionsService = {
+    updatePrecheckEnabled: jest.fn().mockResolvedValue(mockUpdatedAction)
+  }
+
+  beforeAll(async () => {
+    jest.mock('../../server/services/actionsService', () => mockActionsService)
+    createServer = require('../../server/createServer')
+  })
+
+  beforeEach(async () => {
+    server = await createServer()
+    await server.initialize()
+  })
+
+  test('calls the actionsService', async () => {
+    await server.inject(goodRequest)
+    expect(mockActionsService.updatePrecheckEnabled).toHaveBeenCalledWith(actionID, goodRequest.payload.enabled)
+  })
+
+  test('returns the updated data from actionsService', async () => {
+    const response = await server.inject(goodRequest)
+    const payload = JSON.parse(response.payload)
+    expect(payload).toEqual(mockUpdatedAction)
+  })
+
+  test('responds with status code 200 for a well formed request', async () => {
+    const response = await server.inject(goodRequest)
+    expect(response.statusCode).toBe(200)
+  })
+
+  test('responds with status code 400 for a badly formed request payload', async () => {
+    const response = await server.inject(badRequestPayload)
+    expect(response.statusCode).toBe(400)
+  })
+
+  afterAll(() => {
+    jest.unmock('../../server/services/actionsService')
+  })
+
+  afterEach(async () => {
+    await server.stop()
+  })
+})
