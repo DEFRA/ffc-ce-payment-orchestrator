9ae3e9a97f62cf5e1bf34f5b6688fe1a81b10535
Paul Andrews <paul.andrews@defra.gov.uk>
2020-06-18 13:38:35 +0100

    Don't need this file

--- test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-STR="!@£$%^&*(')\`_+\,-={}[]:;\"<>.?/|\\\~" ; echo $STR
-
-#helm upgrade ffc-ce-payment-orchestrator-pr43 --namespace=ffc-ce-payment-orchestrator-pr43 ./helm/ffc-ce-payment-orchestrator  --set image=ssvffcfesac1001.azurecr.io/ffc-ce-payment-orchestrator:pr43,namespace=ffc-ce-payment-orchestrator-pr43,pr=pr43,deployment.redeployOnChange=pr43-137 --set labels.version=pr43 --install --atomic --version=pr43 --set post.username="qqq2!@£$%^&*(')\`_+\,-={}[]:;\"<>.?/|\\\~qqq2"
